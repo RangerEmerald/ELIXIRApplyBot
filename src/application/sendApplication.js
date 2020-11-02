7e31448b94010dc377f3d452fe98856a4dd2eebb
Ranger Emerald <rangeremerald@imac.lan>
2020-11-02 10:00:33 -0700

    New System; v2
    Made it so that it would ask if theh information you sent was correct

--- src/application/sendApplication.js
+++ src/application/sendApplication.js
@@ -12,14 +12,6 @@ function validURL(string) {
     return url.protocol === "http:" || url.protocol === "https:";
 }
 
-async function reject(message, consoles){
-    const warning = await consoles.send(`<@!${message.author.id}> tried to send: ${message.content}`)
-        .then(setTimeout(() => {warning.delete();}, 60000));
-    message.delete();
-    const reply = await message.reply("elixir.apply [your nitrotype profile link] [nitrotype accuracy] [nitrotype wpm]` is the format! Make sure you follow it!")
-        .then(setTimeout(()=>{reply.delete();}, 5000));
-}
-
 async function sendDelete(messages, message, time){
     message.delete();
     const reply = await message.reply(messages)
@@ -27,7 +19,6 @@ async function sendDelete(messages, message, time){
 }
 
 async function sendapply(message, args, Discord){
-    let consoles = message.guild.channels.cache.get(process.env.OFFICER_CHANNEL_ID);
     if(args[0] === "apply"){
         if(args[1]){
             if(validURL(args[1])){
@@ -35,46 +26,60 @@ async function sendapply(message, args, Discord){
                 if(seeRacer === "racer/"){
                     if(args[2]){
                         if(!isNaN(args[2])){
-                            if(args[2] <= 99){
-                                if(args[2] >= 96){
-                                    if(args[3]){
-                                        if(!isNaN(args[3])){
-                                            if(args[3] <= 180){
-                                                if(args[3] >= 60){
-                                                    message.delete();
-                                                    let applicationSend = message.guild.channels.cache.get(process.env.APPLYSEND_CHANNEL_ID);
+                            if(args[3]){
+                                if(!isNaN(args[3])){
+                                    message.delete();
+                                    const isCorrectEmbed = new Discord.MessageEmbed()
+                                        .setColor("ORANGE")
+                                        .setTitle("Is this information correct? Type `yes` if it is; If not, type `no`")
+                                        .setDescription(`**Applicant Nitrotype Profile Link:** ${args[1]}\n**Applicant Accuracy:** ${args[2]}\n**Applicant WPM:** ${args[3]}`)
+                                        .setFooter(`Author ID: ${message.author.id}`)
+                                        .setTimestamp(message.createdAt);
 
-                                                    const appid = await applicationSend.send(`_ _`);
-
-                                                    const secondEmbed = new Discord.MessageEmbed()
-                                                        .setColor("RED")
-                                                        .setAuthor(message.author.tag)
-                                                        .setTitle(`New Application Sent By: ${message.author.tag}\nApplication ID: ${appid.id}`)
-                                                        .setDescription(`**Applicant Nitrotype Profile Link:** ${args[1]}\n**Applicant Accuracy:** ${args[2]}\n**Applicant WPM:** ${args[3]}`)
-                                                        .addField(`Is Application Accepted:`, `Application Pending Review`, true)
-                                                        .setFooter(`Author ID: ${message.author.id}`)
-                                                        .setTimestamp(message.createdAt);
-
-                                                    appid.edit(secondEmbed);
-                                                    const reply = await message.reply("Your application has been recorded. Please be patiant as the officers review your application. Make sure that the format of your application was: `[your nitrotype profile link] [accuracy] [wpm]`. If you made an error in your application, dm one of the online officers. Also, make sure that your DMs are open so that you can be informed when your application has been accpeted or rejected.")
-                                                        .then(setTimeout(()=>{reply.delete();}, 60000));
-                                                } else {
-                                                    reject(message, consoles);
-                                                }
-                                            } else {
-                                                reject(message, consoles);
-                                            }
+                                    const sendIsCorrect = await message.channel.send(isCorrectEmbed);
+                                    const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 30000 });
+                                    collector.on('collect', async message => {
+                                        if(message.content.toLowerCase() === "yes"){
+                                            message.delete();
+                                            collector.stop("yes");
+                                        } else if(message.content.toLowerCase() === "no"){
+                                            message.delete();
+                                            collector.stop("else");
                                         } else {
-                                            sendDelete("Make sure your WPM only consists of numbers! Do not add any extra characters at the end of your WPM!", message, 5000);
+                                            const reply = await message.reply("You most choose either `yes` or `no`!")
+                                                .then(setTimeout(() => {reply.delete();}, 5000));
                                         }
-                                    } else {
-                                        sendDelete("Make sure you include your WPM! The format of the application is: `elixir.apply [your nitrotype profile link] [nitrotype accuracy] [nitrotype wpm]`!", message, 10000);
-                                    }
+                                    });
+                                    collector.on('end', async (collected, reason) => {
+                                        if(reason === "time") return;
+                                        else if(reason === "yes"){
+                                            let applicationSend = message.guild.channels.cache.get(process.env.APPLYSEND_CHANNEL_ID);
+
+                                            const appid = await applicationSend.send(`_ _`);
+
+                                            const secondEmbed = new Discord.MessageEmbed()
+                                                .setColor("RED")
+                                                .setAuthor(message.author.tag)
+                                                .setTitle(`New Application Sent By: ${message.author.tag}\nApplication ID: ${appid.id}`)
+                                                .setDescription(`**Applicant Nitrotype Profile Link:** ${args[1]}\n**Applicant Accuracy:** ${args[2]}\n**Applicant WPM:** ${args[3]}`)
+                                                .addField(`Is Application Accepted:`, `Application Pending Review`, true)
+                                                .setFooter(`Author ID: ${message.author.id}`)
+                                                .setTimestamp(message.createdAt);
+
+                                            appid.edit(secondEmbed);
+                                            const reply = await message.reply("Your application has been recorded. Please be patiant as the officers review your application. Make sure that your DMs are open so that you can be informed when your application has been accpeted or rejected.")
+                                                .then(setTimeout(()=>{reply.delete();}, 60000));
+                                        } else {
+                                            const sendmessage = await message.channel.send("Applicatoin Stopped")
+                                                .then(setTimeout(() => {sendmessage.delete();}, 10000));
+                                        }
+                                        sendIsCorrect.delete();
+                                    });
                                 } else {
-                                    reject(message, consoles);
+                                    sendDelete("Make sure your WPM only consists of numbers! Do not add any extra characters at the end of your WPM!", message, 5000);
                                 }
                             } else {
-                                reject(message, consoles);
+                                sendDelete("Make sure you include your WPM! The format of the application is: `elixir.apply [your nitrotype profile link] [nitrotype accuracy] [nitrotype wpm]`!", message, 10000);
                             }
                         } else {
                             sendDelete("Make sure your accuracy only consists of numbers! Do not add any extra characters at the end of your accuracy!", message, 10000);
