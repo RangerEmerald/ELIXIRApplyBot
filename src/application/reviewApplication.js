0d2a9802b1e8fafd1a6ada305a2a6f708f176f8e
Ranger Emerald <rangeremerald@imac.lan>
2020-11-02 12:36:51 -0700

    Added [reason] option to accepting application
    When accepting an application, you can now add a reason to why you accepted/rejected the application

--- src/application/reviewApplication.js
+++ src/application/reviewApplication.js
@@ -5,7 +5,7 @@ async function reviewapply(message, args, Discord){
         if(args[0] === "application"){
             if(!args[1]){
                 message.delete();
-                const reply = await message.reply("The format for accepting an application is: `elixir.application [application id] [accept/reject]`")
+                const reply = await message.reply("The format for accepting an application is: `elixir.application [application id] [accept/reject] [reason(optional)]`")
                     .then(setTimeout(() => {reply.delete();}, 10000));
             } else if(!isNaN(args[1])){
                 const application = await message.channel.messages.fetch(args[1]);
@@ -13,17 +13,21 @@ async function reviewapply(message, args, Discord){
                     let accrejt = args[2];
                     if((accrejt !== "accept") && (accrejt !== "reject")){
                         message.delete();
-                        const reply = await message.reply("The format for accepting an application is: `elixir.application [applicatoin id] [accept/reject]`")
+                        const reply = await message.reply("The format for accepting an application is: `elixir.application [applicatoin id] [accept/reject] [reason(optional)]`")
                             .then(setTimeout(() => {reply.delete();}, 10000));
                     } else {
                         if(application.embeds[0].author !== null){
                             let authorID = application.embeds[0].footer.text.slice(11);
                             const applicationEmbed = new Discord.MessageEmbed()
                                 .setTitle(`Application Sent By: ${application.embeds[0].author.name}\nApplication ID: ${args[1]}`)
-                                .setDescription(`**Application ${accrejt}ed by <@!${message.author.id}>**`)
                                 .setFooter(application.embeds[0].description)
                                 .setTimestamp(application.embeds[0].timestamp);
 
+                            if(args[3]){
+                                applicationEmbed.setDescription(`**Application ${accrejt}ed by <@!${message.author.id}>**\n**Reason:** ${args[3]}`);
+                            } else {
+                                applicationEmbed.setDescription(`**Application ${accrejt}ed by <@!${message.author.id}>**`);
+                            }
                             let role = message.guild.roles.cache.find(r => r.name.toLowerCase() === process.env.APPLICATION_ROLE);
                             let author = message.guild.members.cache.get(authorID);
                             if(accrejt === "accept"){
@@ -44,7 +48,7 @@ async function reviewapply(message, args, Discord){
 
                             application.edit(applicationEmbed);
                             message.delete();
-                            sendAuthorDM.senddm(authorID, accrejt, message.author.tag, message);
+                            sendAuthorDM.senddm(authorID, accrejt, message.author.tag, message, args, Discord);
                             const reply = await message.reply(`You have ${accrejt}ed Application #${args[1]}`)
                                 .then(setTimeout(() => {reply.delete();}, 10000));
                         } else {
@@ -64,7 +68,7 @@ async function reviewapply(message, args, Discord){
             }
         } else {
             message.delete();
-            const reply = await message.reply("The format for accepting an application is: `elixir.application [applicatoin id] [accept/reject]`")
+            const reply = await message.reply("The format for accepting an application is: `elixir.application [applicatoin id] [accept/reject] [reason(optional)]`")
                 .then(setTimeout(() => {reply.delete();}, 10000));
         }
     } catch(err){
