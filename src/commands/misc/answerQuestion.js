99484dee30a797a1223e19f3aa31582e9ad0cab1
Ranger Emerald <rangeremerald@imac.lan>
2020-11-15 16:23:28 -0700

    Ask if answerquestion is what you want

--- src/commands/misc/answerQuestion.js
+++ src/commands/misc/answerQuestion.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const authorDm = require('./sendAuthorDM.js');
 
-async function aswQuestion(args, message, Discord){
+async function aswQuestion(args, message, Discord, answerQuestionList){
     try{
         if(args[0] === "answerquestion"){
             if(!args[1]){
@@ -14,6 +14,7 @@ async function aswQuestion(args, message, Discord){
                     const answer = message.content.slice(process.env.BOT_PREFIX.length).split(" ").splice(2).join(" ")
                     if(answer){
                         if(question.embeds[0].author !== null){
+                            answerQuestionList[message.author.id] = true;
                             let authorID = question.embeds[0].footer.text.slice(11);
                             const answerEmbed = new Discord.MessageEmbed()
                                 .setTitle(`Question Sent by: ${question.embeds[0].author.name}\nQuestion ID: ${args[1]}`)
@@ -21,11 +22,35 @@ async function aswQuestion(args, message, Discord){
                                 .setDescription(`${question.embeds[0].description}\n**Answer:** ${answer} - Answered by ${message.author.tag}`)
                                 .setTimestamp(message.createdAt);
 
-                            question.edit(answerEmbed);
                             message.delete();
-                            authorDm.authorDM(authorID, message, Discord, answer, message.author.tag, question.embeds[0].description, args);
-                            const reply = await message.reply(`You have answered Question #${args[1]}`)
-                                .then(setTimeout(() => reply.delete(),20000));
+                            const ask = await message.channel.send(`Are you sure you want to reply with: \`${answer}\`. Type \`yes\` to continue. Type \`no\` to stop.`);
+                            const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 30000 });
+                            collector.on('collect', async message => {
+                                if(message.content.toLowerCase() === "yes"){
+                                    collector.stop("yes");
+                                    message.delete();
+                                } else if(message.content.toLowerCase() === "no"){
+                                    collector.stop("no");
+                                    message.delete();
+                                } else {
+                                    message.delete();
+                                    const reply = await message.reply("Please answer with only `yes` or `no`!")
+                                        .then(setTimeout(() => reply.delete(), 20000));
+                                }
+                            });
+                            collector.on('end', async (collected, reason) => {
+                                if(reason === "yes"){
+                                    question.edit(answerEmbed);
+                                    authorDm.authorDM(authorID, message, Discord, answer, message.author.tag, question.embeds[0].description, args);
+                                    const reply = await message.reply(`You have answered Question #${args[1]}`)
+                                        .then(setTimeout(() => reply.delete(),20000));
+                                } else {
+                                    const stopMessage = await message.channel.send("Question answering stopped.")
+                                        .then(setTimeout(() => stopMessage.delete(), 30000));
+                                }
+                                ask.delete();
+                                delete answerQuestionList[message.author.id];
+                            });
                         } else {
                             message.delete();
                             const reply = await message.reply(`The Question #${args[1]} was already reviewed`)
