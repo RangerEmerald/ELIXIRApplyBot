fc3316682c357aab6e6197bcb1c89d17060f6701
Ranger Emerald <rangeremerald@imac.lan>
2020-12-26 15:45:51 -0700

    Made some changes and improvements
    Line 7: If the user is either officer or captain, will ignore their messages. Line 9, 14, 27, 47, 54: Changed brokenAmount to brokenamount. Line 38 - 40: If the time is above the integer signed limit, will set to to the limit/1000 so it won't be set to 1.

--- src/commands/moderation/timeout_limitcommand.js
+++ src/commands/moderation/timeout_limitcommand.js
@@ -4,13 +4,14 @@ const fs = require('fs');
 let limitArray = {};
 
 async function limitcommandusage(message, Discord){
+    if(message.member.roles.cache.find(r => r.name.toLowerCase() === "officer") || message.member.roles.cache.find(r => r.name.toLowerCase() === "captain")) return;
     let date = new Date();
     if(!limitArray[message.author.id]){
-        limitArray[message.author.id] = {"commands": 0, "brokenAmount": 0, "time": 30, "start": 'N/A'};
+        limitArray[message.author.id] = {"commands": 0, "brokenamount": 0, "time": 30, "start": 'N/A'};
     }
     limitArray[message.author.id].commands++;
     if(limitArray[message.author.id].commands >= 5){
-        limitArray[message.author.id].brokenAmount++;
+        limitArray[message.author.id].brokenamount++;
         let role = message.guild.roles.cache.find(r => r.name.toLowerCase() === process.env.JUST_JOINED);
         let role2 = message.guild.roles.cache.find(r => r.name.toLowerCase() === process.env.TIMEOUT_ROLE);
         let consoles = message.guild.channels.cache.get(process.env.LOG_CHANNEL);
@@ -23,7 +24,7 @@ async function limitcommandusage(message, Discord){
                     if(consoles){
                         const timeoutDone = new Discord.MessageEmbed()
                             .setAuthor(message.member.user.tag, message.author.avatarURL())
-                            .setTitle(`Timeout Warning #${limitArray[message.author.id].brokenAmount} Time Served.`)
+                            .setTitle(`Timeout Warning #${limitArray[message.author.id].brokenamount} Time Served.`)
                             .setColor("GREEN")
                             .setFooter(`Author ID: ${message.author.id}`)
                             .setTimestamp(message.createdAt);
@@ -34,20 +35,23 @@ async function limitcommandusage(message, Discord){
                     message.member.roles.remove(role2);
                     limitArray[message.author.id].start = "N/A";
                     limitArray[message.author.id].time*=2;
+                    if(limitArray[message.author.id].time > (2**31-1)/1000){
+                        limitArray[message.author.id].time = (2**31-1)/1000;
+                    }
                 }
             }, limitArray[message.author.id].time*1000));
         }
         if(consoles){
             const timeoutEmbed = new Discord.MessageEmbed()
                 .setAuthor(message.member.user.tag, message.author.avatarURL())
-                .setTitle(`Timeout: Command Usage Failure. Time: ${limitArray[message.author.id].time} Seconds. Warning #${limitArray[message.author.id].brokenAmount}`)
+                .setTitle(`Timeout: Command Usage Failure. Time: ${limitArray[message.author.id].time} Seconds. Warning #${limitArray[message.author.id].brokenamount}`)
                 .setColor("RED")
                 .setFooter(`Author ID: ${message.author.id}`)
                 .setTimestamp(message.createdAt);
                 
             consoles.send(timeoutEmbed);
         }
-        const reply = await message.reply(`The failure of sending an application to ELIXIR ${limitArray[message.author.id].commands} times has resulted in you being temp muted. This is you ${limitArray[message.author.id].brokenAmount} warning. Try again in ${limitArray[message.author.id].time} seconds.`)
+        const reply = await message.reply(`The failure of sending an application to ELIXIR ${limitArray[message.author.id].commands} times has resulted in you being temp muted. This is your ${limitArray[message.author.id].brokenamount} warning. Try again in ${limitArray[message.author.id].time} seconds.`)
             .then(setTimeout(() => reply.delete(), 20000));
         limitArray[message.author.id].commands = 0;
     }
